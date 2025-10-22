d4358bcd9d5aa2d2aae34713f064c0d3ce73e5aa
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-10-22 10:27:49 +0100

    Add authentication handling to layout component with loading state and protected routes

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,9 +1,35 @@
 import { Stack } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function Layout() {
+  const { isSignedIn, isLoaded, userId, sessionId, getToken } = useAuth();
+
+  console.log('Auth State:', {
+    isSignedIn: isSignedIn as boolean,
+    isLoaded: isLoaded,
+    userId: userId,
+    sessionId: sessionId,
+  });
+
+  if (!isLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color={'#0000ff'} />
+      </View>
+    );
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Protected guard={isSignedIn as boolean}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack.Protected>
     </Stack>
   );
 }
