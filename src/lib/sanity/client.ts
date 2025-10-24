b69ce42ced9f3e5b09a30514c7b406a115d4ae9c
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-10-24 08:43:56 +0100

    feat: add Sanity client configuration and image URL builder

---
+++ src/lib/sanity/client.ts
@@ -0,0 +1,26 @@
+import createClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+// client safe configuration
+export const config = {
+  projectId: '5r500ptx',
+  dataset: 'production',
+  apiVersion: '2024-01-01',
+  useCdn: false,
+}
+
+// admin level client, used for server side operations
+// admin client for mutations
+const adminConfig = {
+  ...config,
+  token: process.env.SANITY_API_TOKEN,
+}
+
+export const client = createClient(config)
+export const adminClient = createClient(adminConfig)
+
+// image URL builder
+const builder = imageUrlBuilder(client)
+export const urlFor = (source: string) => {
+  return builder.image(source)
+}
