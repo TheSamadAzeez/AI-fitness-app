b5ea2d39c4878183b27d30f890f20e2015074009
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-02 15:09:30 +0100

    feat: implement exercise list with search and refresh functionality in ExerciseSelectionModal

--- app/(app)/components/exercise-modal.tsx
+++ app/(app)/components/exercise-modal.tsx
@@ -1,10 +1,21 @@
+import { client } from '@/src/lib/sanity/client';
 import { Exercise } from '@/src/lib/sanity/types';
 import { useWorkoutStore } from '@/store/workout-store';
 import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import { useState } from 'react';
-import { View, Text, Modal, StatusBar, TouchableOpacity, TextInput } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  FlatList,
+  Modal,
+  RefreshControl,
+  StatusBar,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { exercisesQuery } from '../(tabs)/exercises';
+import ExerciseCard from './exercise-card';
 
 interface ExerciseSelectionModalProps {
   visible: boolean;
@@ -12,12 +23,54 @@ interface ExerciseSelectionModalProps {
 }
 
 export function ExerciseSelectionModal({ visible, onClose }: ExerciseSelectionModalProps) {
-  const router = useRouter();
   const { addExerciseToWorkout } = useWorkoutStore();
   const [exercises, setExercises] = useState<Exercise[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredExercises, setFilteredExercises] = useState<Exercise[]>([]);
   const [refreshing, setRefreshing] = useState(false);
+
+  //   if visible is true, fetch exercises
+  useEffect(() => {
+    if (visible) {
+      fetchExercises();
+    }
+  }, [visible]);
+
+  // Filter exercises based on search query
+  useEffect(() => {
+    if (searchQuery.trim() === '') {
+      setFilteredExercises(exercises);
+    } else {
+      const filtered = exercises.filter((exercise) =>
+        exercise.name?.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredExercises(filtered);
+    }
+  }, [searchQuery, exercises]);
+
+  const fetchExercises = async () => {
+    try {
+      const exercises = await client.fetch(exercisesQuery);
+
+      setExercises(exercises);
+      setFilteredExercises(exercises);
+    } catch (error) {
+      console.error('Error fetching exercises:', error);
+    }
+  };
+
+  const handleExercisePress = (exercise: Exercise) => {
+    // Add the selected exercise to the workout store in Zustand
+    addExerciseToWorkout({ name: exercise.name as string, sanityId: exercise._id as string });
+    onClose();
+  };
+
+  const onRefresh = async () => {
+    // Implement refresh logic here
+    setRefreshing(true);
+    await fetchExercises();
+    setRefreshing(false);
+  };
 
   return (
     <Modal
@@ -57,6 +110,44 @@ export function ExerciseSelectionModal({ visible, onClose }: ExerciseSelectionMo
         </View>
 
         {/* exercise list  */}
+        <FlatList
+          data={filteredExercises}
+          renderItem={({ item }) => {
+            return (
+              <ExerciseCard
+                item={item}
+                onPress={() => {
+                  handleExercisePress(item);
+                }}
+                showChevron={false}
+              />
+            );
+          }}
+          keyExtractor={(item) => item._id}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingTop: 16, paddingHorizontal: 16, paddingBottom: 32 }}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors={['#3B82F6']}
+              tintColor={'#3B82F6'}
+            />
+          }
+          ListEmptyComponent={
+            <View className="flex-1 items-center justify-center py-20">
+              <Ionicons name="fitness-outline" size={64} color="#D1D5DB" />
+              <Text className="mt-4 text-lg font-semibold text-gray-400">
+                {searchQuery ? 'No exercises found' : 'Loading exercises...'}
+              </Text>
+              <Text className="mt-2 text-sm text-gray-400">
+                {searchQuery
+                  ? 'Try adjusting your search terms.'
+                  : 'Please wait while we load the exercises.'}
+              </Text>
+            </View>
+          }
+        />
       </SafeAreaView>
     </Modal>
   );
